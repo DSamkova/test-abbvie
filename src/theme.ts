6289d2c8c4c3679364407499960b5b1a1769b156
DSamkova <dsamkova@n-ix.com>
2024-03-12 10:07:33 +0200

    feat: theme

---
+++ src/theme.ts
@@ -0,0 +1,32 @@
+export const GlobalTheme = {
+    typography: {
+        fontWeight: 600,
+        lineHeight: 1.57143,
+        fontSize: 14,
+        fontFamily: [
+            'Public Sans', // Replace this with your desired font family
+            'sans-serif',
+        ].join(','),
+        h6: {
+            fontSize: 18,
+            fontWeight: 700,
+        },
+    },
+    palette: {
+        primary: {
+            main: '#212B36',
+        },
+        secondary: {
+            main: '#00B8D9',
+        },
+        success: {
+            main: '#00A66E',
+        },
+        warning: {
+            main: '#FFAB00'
+        },
+        error: {
+            main: '#FF5530',
+        },
+    }
+}
