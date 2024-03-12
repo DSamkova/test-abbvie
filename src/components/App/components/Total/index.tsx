ce91a1c11c254bc847ffcc486a6b8d2472868911
DSamkova <dsamkova@n-ix.com>
2024-03-12 10:10:08 +0200

    feat: Total

---
+++ src/components/App/components/Total/index.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import classes from './Total.module.scss';
+import {defaultData, TotalSections} from './data';
+import {styled} from '@mui/material/styles';
+import {Typography as MuiTypography, TypographyProps} from '@mui/material';
+import EjectIcon from '@mui/icons-material/Eject';
+import {Chart} from 'react-google-charts';
+import Paper from '../../../Paper';
+
+const Welcome: React.FC = () => {
+    const options = {
+        legend: {position: 'none'},
+        hAxis: {textPosition: 'none'},
+        vAxis: {
+            textPosition: 'none',
+            gridlines: {
+                color: 'transparent',
+            },
+        },
+        baselineColor: 'transparent',
+        chartArea: {width: '100%', height: '100%'},
+    };
+    return (
+        <div className={classes.wrapper}>
+            {TotalSections?.map((item) => {
+                return (
+                    <Paper className={classes.total_item} key={item.id}>
+                        <div className={classes.total_info}>
+                            <Typography>{item.title}</Typography>
+                            <Typography mt={2} mb={1} gap={1} display='flex' alignItems={'center'}>
+                                {
+                                    item.change >= 0 ?
+                                        <><EjectIcon color='success'/>+</>
+                                        : <EjectIcon color='error' style={{transform: 'rotate(180deg)'}}/>
+                                }
+                                {item.change}%
+                            </Typography>
+                            <Typography variant='h3'>{item.total}</Typography>
+                        </div>
+                        <div>
+                            <Chart
+                                chartType='ColumnChart'
+                                options={{...options, colors: [item.color]}}
+                                width='60px'
+                                height='60px'
+                                data={defaultData}
+                            />
+                        </div>
+                    </Paper>
+                )
+            })}
+        </div>
+
+    );
+}
+
+const Typography = styled(MuiTypography)<TypographyProps>(() => ({
+    '&.MuiTypography-h3': {
+        fontWeight: 700,
+        fontSize: '1.875rem',
+    },
+}));
+
+
+export default Welcome;
---
+++ src/components/Paper/index.tsx
@@ -0,0 +1,19 @@
+import {Paper as MuiPaper, PaperProps, SxProps} from '@mui/material';
+import {styled} from '@mui/material/styles';
+
+const Paper = styled(MuiPaper)<PaperProps & SxProps>(() => ({
+    backgroundColor: 'rgb(255, 255, 255)',
+    color: 'rgb(33, 43, 54)',
+    transition: 'box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms',
+    backgroundImage: 'none',
+    overflow: 'hidden',
+    position: 'relative',
+    boxShadow: 'rgba(145, 158, 171, 0.2) 0px 0px 2px 0px, rgba(145, 158, 171, 0.12) 0px 12px 24px -4px',
+    borderRadius: '16px',
+    zIndex: '0',
+    display: 'flex',
+    alignItems: 'center',
+    padding: '24px',
+}));
+
+export default Paper;
